16a25229f1d6fa6f87bdba9cf4ef54b3dd4d14fd
DanishDahaka <nito17ae@student.cbs.dk>
2021-08-15 13:18:56 +0200

    add reverse_numbers.py

---
+++ algorithms/reverse_numbers.py
@@ -0,0 +1,22 @@
+def reverse_divisable(number, divisor) -> bool:
+    
+    string_format = str(number)
+    
+    reversed_number = int(string_format[::-1])
+    print(reversed_number)
+    
+    if (number - reversed_number) % divisor == 0:
+        
+        return True
+    
+    return False
+    
+
+def beautifulDays(i, j, k) -> int:
+            
+    return len([1 for num in range(i,j+1) if reverse_divisable(num, k)])
+
+
+if __name__ == '__main__':
+
+    print(beautifulDays(20,23,6))
