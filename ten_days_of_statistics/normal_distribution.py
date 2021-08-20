fbf6080e31b119a430d66de1504f7d80b8b477e1
DanishDahaka <nito17ae@student.cbs.dk>
2021-08-20 11:19:14 +0200

    move prints to main method

--- ten_days_of_statistics/normal_distribution.py
+++ ten_days_of_statistics/normal_distribution.py
@@ -2,34 +2,39 @@ import math
 
 # adapted after code from augustocmen
 
-mean, std = 20, 2
 
 def cdf_normal_distribution(mean, std, x):
 
     return 0.5 * (1 + math.erf((x - mean) / (std * (2 ** 0.5))))
 
-# Less than 19.5
-print(f'{cdf_normal_distribution(mean, std, 19.5):.3f}')
-# Between 20 and 22
-print(f'{cdf_normal_distribution(mean, std, 22) - cdf_normal_distribution(mean, std, 20):.3f}')
-
-### Task 2 ###
-
-# beware, wished output is different, e.g. percentage 10.25
 
 def cdf_normal_distribution_percentage(mean, std, x):
 
     return (0.5 * (1 + math.erf((x - mean) / (std * (2 ** 0.5))))) * 100
 
-### Task 2 ###
 
-mean, std = 70, 10
 
-# higher than 80
-print(f'{100-cdf_normal_distribution_percentage(mean, std, 80):.2f}')
+if __name__ == '__main__':
 
-# equal or larger 60
-print(f'{100-cdf_normal_distribution_percentage(mean, std, 60):.2f}')
+    ### Task 1 ###
 
-# less than 60
-print(f'{cdf_normal_distribution_percentage(mean, std, 60):.2f}')
+    mean, std = 20, 2
+
+    # Less than 19.5
+    print(f'{cdf_normal_distribution(mean, std, 19.5):.3f}')
+    # Between 20 and 22
+    print(f'{cdf_normal_distribution(mean, std, 22) - cdf_normal_distribution(mean, std, 20):.3f}')
+
+
+    ### Task 2 ###
+    mean, std = 70, 10
+
+    # beware, wished output for task 2 is different, e.g. percentage 10.25  
+    # higher than 80
+    print(f'{100-cdf_normal_distribution_percentage(mean, std, 80):.2f}')
+
+    # equal or larger 60
+    print(f'{100-cdf_normal_distribution_percentage(mean, std, 60):.2f}')
+
+    # less than 60
+    print(f'{cdf_normal_distribution_percentage(mean, std, 60):.2f}')
