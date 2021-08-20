ed8c7057a89cc5b2437cfa4fdc62c998522a8c1f
DanishDahaka <nito17ae@student.cbs.dk>
2021-08-20 11:15:00 +0200

    refactor to re-use functions

--- ten_days_of_statistics/normal_distribution.py
+++ ten_days_of_statistics/normal_distribution.py
@@ -4,28 +4,32 @@ import math
 
 mean, std = 20, 2
 
-cdf = lambda x: 0.5 * (1 + math.erf((x - mean) / (std * (2 ** 0.5))))
+def cdf_normal_distribution(mean, std, x):
+
+    return 0.5 * (1 + math.erf((x - mean) / (std * (2 ** 0.5))))
 
 # Less than 19.5
-print(f'{cdf(19.5):.3f}')
+print(f'{cdf_normal_distribution(mean, std, 19.5):.3f}')
 # Between 20 and 22
-print(f'{cdf(22) - cdf(20):.3f}')
+print(f'{cdf_normal_distribution(mean, std, 22) - cdf_normal_distribution(mean, std, 20):.3f}')
 
 ### Task 2 ###
 
 # beware, wished output is different, e.g. percentage 10.25
 
-cdf_percentage = lambda x: (0.5 * (1 + math.erf((x - mean) / (std * (2 ** 0.5))))) * 100
+def cdf_normal_distribution_percentage(mean, std, x):
+
+    return (0.5 * (1 + math.erf((x - mean) / (std * (2 ** 0.5))))) * 100
 
 ### Task 2 ###
 
 mean, std = 70, 10
 
 # higher than 80
-print(f'{100-cdf_percentage(80):.2f}')
+print(f'{100-cdf_normal_distribution_percentage(mean, std, 80):.2f}')
 
 # equal or larger 60
-print(f'{100-cdf_percentage(60):.2f}')
+print(f'{100-cdf_normal_distribution_percentage(mean, std, 60):.2f}')
 
 # less than 60
-print(f'{cdf_percentage(60):.2f}')
+print(f'{cdf_normal_distribution_percentage(mean, std, 60):.2f}')
